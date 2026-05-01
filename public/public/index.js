ec9a45ef5bac97c8db60118457c353eca2c583c3
toviman00-web <toviman00@gmail.com>
2026-05-01 18:46:49 +0300

    Delete public directory

--- public/public/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const express = require('express');
-
-const app = express();
-
-// Цей рядок говорить серверу віддавати файли з папки public:
-app.use(express.static(path.join(__dirname, 'public')));
